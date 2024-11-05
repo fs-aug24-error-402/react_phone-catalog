308225ed2080c93b71007d3298322d470a01fff5
Mariia <maria.gula0307@gmail.com>
2024-11-05 23:26:01 +0200

    added App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { AnimatePresence } from 'framer-motion';
+
 import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header';
+import { BurgerMenu } from './components/BurgerMenu';
 
 function App() {
+  const [isAsideVisible, setIsAsideVisible] = useState(false);
+  const location = useLocation();
+
+  const handleToggleAside = () => setIsAsideVisible(prev => !prev);
+
+  useEffect(() => {
+    setIsAsideVisible(false);
+  }, [location]);
+
   return (
     <div className="app flex flex-col min-h-screen">
-      <header></header>
+      <Header
+        isAsideVisible={isAsideVisible}
+        onToggleAside={handleToggleAside}
+      />
+
+      <AnimatePresence>{isAsideVisible && <BurgerMenu />}</AnimatePresence>
 
       <main className="flex-1">
         <Outlet />
