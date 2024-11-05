7f1dab2d54f77cdaf6854867a45eb9f3d1f65602
Mariia <maria.gula0307@gmail.com>
2024-11-05 13:18:20 +0200

    configure tailwind and changed public

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,82 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
 
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    screens: {
+      mobile: '320px',
+      tablet: '640px',
+      desktop: '1200px',
+    },
+
+    spacing: {
+      '8': '8px',
+      '13': '13px',
+      '16': '16px',
+      '18': '18px',
+      '22': '22px',
+      '24': '24px',
+      '32': '32px',
+      '56': '56px',
+      '64': '64px',
+      '72': '72px',
+    },
+
+    extend: {
+      colors: {
+        accent: '#4219D0',
+        'secondary-accent': '#F4BA47',
+        primary: '#0F0F11',
+        secondary: '#89939A',
+        icons: '#B4BDC3',
+        elements: '#E2E6E9',
+        'hover-and-bg': '#FAFBFC',
+        white: '#FFFFFF',
+        green: '#27AE60',
+        red: '#EB5757',
+      },
+
+      borderRadius: {
+        sm: '8px',
+        lg: '48px',
+      },
+    },
+
+    fontFamily: {
+      sans: ['Mont', ...defaultTheme.fontFamily.sans],
+    },
+
+    fontSize: {
+      small: [
+        '12px',
+        {
+          lineHeight: '15px',
+          letterSpacing: '0',
+          fontWeight: '600',
+        },
+      ],
+
+      buttons: [
+        '14px',
+        {
+          lineHeight: '21px',
+          letterSpacing: '0',
+          fontWeight: '600',
+        },
+      ],
+
+      uppercase: [
+        '12px',
+        {
+          lineHeight: '11px',
+          letterSpacing: '0.04em',
+          fontWeight: '700',
+        },
+      ],
+    },
+  },
+
+  plugins: [],
+};
