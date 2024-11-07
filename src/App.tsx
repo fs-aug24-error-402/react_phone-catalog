d49700664b846fb2bbe1aacdab507cc0d1ba0d35
Michael Sulima <sulima719@gmail.com>
2024-11-07 02:17:33 +0200

    slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,34 @@
 import { Outlet } from 'react-router-dom';
 import { Footer } from './components/Footer/Footer';
-import { Card } from './components/Card';
-import { Product } from './types/Product';
+import SwiperItem from './components/SwiperItem/SwiperItem';
+// import { Card } from './components/Card';
+// import { Product } from './types/Product';
 
-const item: Product = {
-  id: 1,
-  category: 'phones',
-  itemId: 'apple-iphone-7-32gb-black',
-  name: 'Apple iPhone 7 32GB Black',
-  fullPrice: 400,
-  price: 375,
-  screen: "4.7' IPS",
-  capacity: '32GB',
-  color: 'black',
-  ram: '2GB',
-  year: 2016,
-  image: 'img/phones/apple-iphone-7/black/00.webp',
-};
+// import React, { useRef, useState } from 'react';
+// Import Swiper React components
+
+// const item: Product = {
+//   id: 1,
+//   category: 'phones',
+//   itemId: 'apple-iphone-7-32gb-black',
+//   name: 'Apple iPhone 7 32GB Black',
+//   fullPrice: 400,
+//   price: 375,
+//   screen: "4.7' IPS",
+//   capacity: '32GB',
+//   color: 'black',
+//   ram: '2GB',
+//   year: 2016,
+//   image: 'img/phones/apple-iphone-7/black/00.webp',
+// };
 
 function App() {
   return (
     <div className="app flex flex-col min-h-screen">
       <header></header>
-      <Card product={item} />
+      <SwiperItem />
+
+      {/* <Card product={item} /> */}
 
       <main className="flex-1">
         <Outlet />
---
+++ src/components/SwiperItem/SwiperItem.tsx
@@ -0,0 +1,57 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import './SwiperItem.scss';
+
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+export default function SwiperItem() {
+  return (
+    <Swiper
+      spaceBetween={100}
+      updateOnWindowResize={true}
+      simulateTouch={true}
+      loop={true}
+      speed={800}
+      autoplay={{
+        delay: 8000,
+        disableOnInteraction: false,
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      navigation={true}
+      modules={[Autoplay, Pagination, Navigation]}
+      className="mySwiper"
+    >
+      <SwiperSlide>
+        <a href="#">
+          <img
+            src="public/img/iphone14banner.png"
+            className="slide"
+            alt="iPhone 14 Pro"
+          />
+        </a>
+      </SwiperSlide>
+      <SwiperSlide>
+        <a href="#">
+          <img
+            src="public/img/iphone14banner.png"
+            className="slide"
+            alt="iPhone 14 Pro"
+          />
+        </a>
+      </SwiperSlide>
+      <SwiperSlide>
+        <a href="#">
+          <img
+            src="public/img/iphone14banner.png"
+            className="slide"
+            alt="iPhone 14 Pro"
+          />
+        </a>
+      </SwiperSlide>
+    </Swiper>
+  );
+}
