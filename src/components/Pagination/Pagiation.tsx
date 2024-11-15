90dc675dac15cec6ea86086b0f0adef5af2b4ee1
Mariia <maria.gula0307@gmail.com>
2024-11-15 04:54:13 +0200

    fix skeleton add not found image in catalog fix filtration

---
+++ src/components/Pagination/Pagiation.tsx
@@ -0,0 +1,95 @@
+import { FC } from 'react';
+import ReactPaginate from 'react-paginate';
+import { useSearchParams } from 'react-router-dom';
+import classNames from 'classnames';
+
+import { Product } from '../../types/Product';
+import { getFilteredDevices } from '../../utils/utils';
+import { getSearchWith } from '../../utils/searchHelper';
+import { useWindowWidth } from '../../app/hooks';
+
+interface Props {
+  items: Product[];
+}
+
+export const PaginatedItems: FC<Props> = ({ items }) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const { isMobile } = useWindowWidth();
+
+  const displayedPagesRange = isMobile ? 1 : 3;
+
+  const query = searchParams.get('query') || '';
+  const sortBy = searchParams.get('sort-by') || '';
+
+  const filteredItems = getFilteredDevices(items, query, sortBy);
+
+  const itemsPerPage = Number(searchParams.get('items-on-page')) || 12;
+  const currentPageNumber = Number(searchParams.get('page-number')) || 1;
+  const pageCount = Math.ceil(filteredItems.length / itemsPerPage);
+
+  const isItFirstPage = currentPageNumber === 1;
+  const isItLastPage = currentPageNumber === pageCount;
+
+  const handlePageClick = (event: { selected: number }) => {
+    setSearchParams(() =>
+      getSearchWith(searchParams, {
+        'page-number': String(event.selected + 1),
+      }),
+    );
+  };
+
+  return (
+    <>
+      <ReactPaginate
+        className="flex items-center gap-8 justify-center"
+        pageLinkClassName="flex items-center justify-center border
+          rounded-[50%] h-32 w-32 leading-none text-center text-none
+          hover:border-primary"
+        activeLinkClassName="bg-primary border-primary text-white
+          pointer-events-none"
+        breakLinkClassName="pointer-events-none"
+        breakLabel={
+          <button
+            disabled
+            className="flex items-center pointer-events-none justify-center
+            border rounded-full hover:border-primary h-32 w-32"
+          >
+            ...
+          </button>
+        }
+        nextLabel={
+          <img
+            src={
+              isItLastPage
+                ? 'img/icons/svg/icon-arrow-right-inactive.svg'
+                : 'img/icons/svg/icon-arrow-right.svg'
+            }
+          />
+        }
+        nextLinkClassName={classNames(
+          'flex justify-center items-center ml-8 rounded-full border',
+          'border-icons hover:border-primary h-32 w-32',
+          { 'pointer-events-none': isItLastPage },
+        )}
+        onPageChange={handlePageClick}
+        marginPagesDisplayed={displayedPagesRange}
+        pageCount={pageCount}
+        previousLabel={
+          <img
+            src={
+              isItFirstPage
+                ? 'img/icons/svg/icon-arrow-left-inactive.svg'
+                : 'img/icons/svg/icon-arrow-left.svg'
+            }
+          />
+        }
+        previousLinkClassName={classNames(
+          'flex justify-center items-center ml-8 rounded-full border',
+          'border-icons hover:border-primary h-32 w-32',
+          { 'pointer-events-none': isItFirstPage },
+        )}
+        renderOnZeroPageCount={null}
+      />
+    </>
+  );
+};
